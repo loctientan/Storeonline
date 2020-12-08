f9ce596fe8c626acbde5bc3c9e57d4d131a5e1bc
thienlamins99 <68331537+thienlamins99@users.noreply.github.com>
2020-12-08 22:12:29 +0700

    Thêm tìm kiếm HomeController, View Index

--- Storeonline/Controllers/HomeController.cs
+++ Storeonline/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Storeonline.Controllers
         private connectDB db = new connectDB();
 
 
-        public ActionResult Index(string id, int? page)
+        public ActionResult Index(string id, int? page, string ten, string price)
         {
             int pageSize = 8;
             int pageNumber = (page ?? 1);
@@ -32,6 +32,15 @@ namespace Storeonline.Controllers
                               select r).OrderBy(x => x.ProductID);
                 return View(result.ToPagedList(pageNumber, pageSize));
             }
+            if (!String.IsNullOrEmpty(ten) || !String.IsNullOrEmpty(price))
+            {
+                var result = (from r in db.Products
+                              where r.ProductName.Contains(ten)
+                              && r.Price.ToString().Contains(price)
+                              select r).OrderBy(a => a.ProductID);
+                return View(result.ToPagedList(pageNumber, pageSize));
+            }
+
             var links = (from l in db.Products
                          select l).OrderBy(x => x.ProductID);
             return View(links.ToPagedList(pageNumber, pageSize));
