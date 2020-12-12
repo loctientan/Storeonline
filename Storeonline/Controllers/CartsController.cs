b2d2911dd78cdab4145a867c8d324eb3b485e91c
tanloc <loctientan@gmail.com>
2020-12-12 16:17:10 +0700

    Update Carts Controller and Details Products

--- Storeonline/Controllers/CartsController.cs
+++ Storeonline/Controllers/CartsController.cs
@@ -79,16 +79,26 @@ namespace Storeonline.Areas.admin.Controllers
 
         public ActionResult Pay(FormCollection form)
         {
-            User user = new User();
-            if(user.Username == null)
+           
+            
+            try
             {
-                user.CustomerNotAccount = true;
+                User user = new User();
+                string sMonth = DateTime.Now.Month.ToString();
+                string sDay = DateTime.Now.Day.ToString();
+                string sMin = DateTime.Now.Minute.ToString();
+                string sMili = DateTime.Now.Millisecond.ToString();
+                string Code = sMonth + sDay + sMin + sMili;
+                
+                //user.CustomerNotAccount = true;
                 FunctionCart functionCart = Session["FunctionCart"] as FunctionCart;
                 Invoice invoice = new Invoice();
                 PaymentCategory paymentCategory = new PaymentCategory();
                 invoice.CreateDate = DateTime.Now;
-                //User user = Session["Username"] as User;
-                invoice.UserID = user.UserID;
+                invoice.InvoiceCode = "HD" + Code;       
+                //user = Session["Username"] as User;                
+                
+                //invoice.UserID = user.UserID;
                 invoice.UserCode = user.UserCode;
                 invoice.Email = form["email"];
                 invoice.LastName = form["lastname"];
@@ -96,6 +106,7 @@ namespace Storeonline.Areas.admin.Controllers
                 invoice.Phone = form["phone"];
                 invoice.Address = form["address"];
                 invoice.ghichu = form["ghichu"];
+                invoice.Payment = 1;
                 //invoice.Payment = paymentCategory.paymentcategoryID;
                 //invoice.PaymentCategory.paymentstatus = form.AllKeys.Single();
                 db.Invoices.Add(invoice);
@@ -104,60 +115,61 @@ namespace Storeonline.Areas.admin.Controllers
                 {
                     InvoiceDetails invoiceDetails = new InvoiceDetails();
 
-                    invoiceDetails.InvoiceID = invoice.InvoiceID;
+                    invoiceDetails.InvoiceID = invoice.InvoiceID;   
                     invoiceDetails.UserCode = invoice.UserCode;
                     invoiceDetails.ProductID = item.Product.ProductID;
                     invoiceDetails.ProductName = item.Product.ProductName;
                     invoiceDetails.Price = item.Product.Price;
                     invoiceDetails.PromotionPrice = item.Product.PromotionPrice;
-                    invoiceDetails.Quantity = item.Quantity;
+                    invoiceDetails.Quantity = item.Quantity;                  
                     db.InvoiceDetails.Add(invoiceDetails);
+              
                 }
 
                 db.SaveChanges();
                 functionCart.RemoveCartAll();
                 SetAlert("Bộ phận bán hàng sẽ liên hệ bạn trong vòng 12h tới. Xin cảm ơn bạn đã mua hàng.", "success");
-                return RedirectToAction("Details", "InvoiceDetails");
+                return RedirectToAction("Details", "Invoices", new { id = invoice.InvoiceID});
             }
-            else if(user.Username != null)
-            {
-                FunctionCart functionCart = Session["FunctionCart"] as FunctionCart;
-                Invoice invoice = new Invoice();
-                PaymentCategory paymentCategory = new PaymentCategory();
-                invoice.CreateDate = DateTime.Now;
-                user = Session["Username"] as User;
-                invoice.UserID = user.UserID;
-                invoice.UserCode = user.UserCode;
-                invoice.Email = user.Email;
-                invoice.LastName = user.LastName;
-                invoice.FirstName = user.FirstName;
-                invoice.Phone = user.Phone;
-                invoice.Address = user.Address;
-                invoice.ghichu = form["ghichu"];
-                //invoice.Payment = paymentCategory.paymentcategoryID;
-                //invoice.PaymentCategory.paymentstatus = form.AllKeys.Single();
-                db.Invoices.Add(invoice);
+            //else if(user.Username != null)
+            //{
+            //    FunctionCart functionCart = Session["FunctionCart"] as FunctionCart;
+            //    Invoice invoice = new Invoice();
+            //    PaymentCategory paymentCategory = new PaymentCategory();
+            //    invoice.CreateDate = DateTime.Now;
+            //    user = Session["Username"] as User;
+            //    invoice.UserID = user.UserID;
+            //    invoice.UserCode = user.UserCode;
+            //    invoice.Email = user.Email;
+            //    invoice.LastName = user.LastName;
+            //    invoice.FirstName = user.FirstName;
+            //    invoice.Phone = user.Phone;
+            //    invoice.Address = user.Address;
+            //    invoice.ghichu = form["ghichu"];
+            //    //invoice.Payment = paymentCategory.paymentcategoryID;
+            //    //invoice.PaymentCategory.paymentstatus = form.AllKeys.Single();
+            //    db.Invoices.Add(invoice);
 
-                foreach (var item in functionCart.carts)
-                {
-                    InvoiceDetails invoiceDetails = new InvoiceDetails();
+            //    foreach (var item in functionCart.carts)
+            //    {
+            //        InvoiceDetails invoiceDetails = new InvoiceDetails();
 
-                    invoiceDetails.InvoiceID = invoice.InvoiceID;
-                    invoiceDetails.UserCode = invoice.UserCode;
-                    invoiceDetails.ProductID = item.Product.ProductID;
-                    invoiceDetails.ProductName = item.Product.ProductName;
-                    invoiceDetails.Price = item.Product.Price;
-                    invoiceDetails.PromotionPrice = item.Product.PromotionPrice;
-                    invoiceDetails.Quantity = item.Quantity;
-                    db.InvoiceDetails.Add(invoiceDetails);
-                }
+            //        invoiceDetails.InvoiceID = invoice.InvoiceID;
+            //        invoiceDetails.UserCode = invoice.UserCode;
+            //        invoiceDetails.ProductID = item.Product.ProductID;
+            //        invoiceDetails.ProductName = item.Product.ProductName;
+            //        invoiceDetails.Price = item.Product.Price;
+            //        invoiceDetails.PromotionPrice = item.Product.PromotionPrice;
+            //        invoiceDetails.Quantity = item.Quantity;
+            //        db.InvoiceDetails.Add(invoiceDetails);
+            //    }
 
-                db.SaveChanges();
-                functionCart.RemoveCartAll();
-                SetAlert("Bộ phận bán hàng sẽ liên hệ bạn trong vòng 12h tới. Xin cảm ơn bạn đã mua hàng.", "success");
-                return RedirectToAction("Details", "InvoiceDetails");
-            }
-            else
+            //    db.SaveChanges();
+            //    functionCart.RemoveCartAll();
+            //    SetAlert("Bộ phận bán hàng sẽ liên hệ bạn trong vòng 12h tới. Xin cảm ơn bạn đã mua hàng.", "success");
+            //    return RedirectToAction("Details", "InvoiceDetails");
+            //}
+            catch
             { 
                 SetAlert("Đặt hàng không thành công. Vui lòng đặt lại.", "danger");
                 return RedirectToAction("ViewCart", "Carts");
